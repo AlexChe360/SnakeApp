dac9141f71f398f59f71ab03d370f5044372f5ca
Alexey <devalex360@gmail.com>
2024-02-07 23:05:17 +0600

    Initial Commit

---
+++ SnakeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnakeApp
+//
+//  Created by  Alexey on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
